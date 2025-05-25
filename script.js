939c399e01f4580cbff2da01da5b4ed85a49dcc0
KieranYMCA <kieran@ymcascotland.org>
2025-05-25 13:00:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
-function startQuiz(id) {
-    var quiz = document.getElementById(id);
-    if (quiz.style.display === "none") {
-        quiz.style.display = "block";
-    } else {
-        quiz.style.display = "none";
-    }
+function checkAnswer(button, isCorrect) {
+  if (isCorrect) {
+    button.classList.add("correct");
+    button.innerText += " - Correct!";
+  } else {
+    button.classList.add("wrong");
+    button.innerText += " - Try again.";
+  }
+  // Disable all buttons in this quiz
+  const buttons = button.parentElement.querySelectorAll("button");
+  buttons.forEach(btn => btn.disabled = true);
 }
